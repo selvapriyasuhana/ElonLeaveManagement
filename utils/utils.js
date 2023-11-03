7b6f179f3fa666f78edd290415b9ff7601c1935a
selvapriyasuhana <142295945+selvapriyasuhana@users.noreply.github.com>
2023-11-03 17:57:45 +0530

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,12 @@
-const jwt = require("jsonwebtoken");
+/*const jwt = require("jsonwebtoken");
 const generateToken = (user) =>
   jwt.sign({ id: user.id }, process.env.SECRET_KEY, { expiresIn: "5m" });
-module.exports = generateToken;
+module.exports = generateToken;*/
+function generateRandomToken() {
+  return Math.random().toString(36).substr(2, 10); // Generate a random token
+}
+
+module.exports = {
+  generateRandomToken,
+};
+
