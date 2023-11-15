008e725b196060c9fabc94434ec2f7b357e55a5b
selvapriyasuhana <142295945+selvapriyasuhana@users.noreply.github.com>
2023-11-15 10:24:22 +0530

    Update Leavecontroller.js

--- Controller/Leavecontroller.js
+++ Controller/Leavecontroller.js
@@ -79,7 +79,7 @@ exports.look = async (req, res) => {
     });
   }
 };
-const mongoose = require("mongoose");
+/*const mongoose = require("mongoose");
 const Staffmodel = require("../Models/Staffmodel.js");
 const Leavemodel = require("../Models/Leavemodel.js");
 exports.update = async (req, res) => {
@@ -213,7 +213,7 @@ exports.update = async (req, res) => {
       }*/
 
       // Save the updated user record in the "users" collection
-      if (Leavetype === "Menstrualleaves" && user.Gender !== "Female") {
+    /*  if (Leavetype === "Menstrualleaves" && user.Gender !== "Female") {
         return res.json({
           status: "Error",
           message: "Menstrual leave is only applicable for women",
@@ -287,7 +287,7 @@ exports.update = async (req, res) => {
     }*/
 
     // Update the leave record in the "leaves" collection using Mongoose
-    const leave = await Leavemodel.findByIdAndUpdate(
+  /*  const leave = await Leavemodel.findByIdAndUpdate(
       user_id,
       {
         Command,
@@ -307,6 +307,188 @@ exports.update = async (req, res) => {
       status: "Error",
       message: "An error occurred while processing the request",
     });
+  }
+};*/
+
+// const mongoose = require("mongoose");
+const Staffmodel = require("../Models/Staffmodel.js");
+const Leavemodel = require("../Models/Leavemodel.js");
+
+exports.update = async (req, res) => {
+  try {
+    const { user_id } = req.params;
+    const { Command, Status, Leavetype, Numberofdays } = req.body;
+
+    // Update the leave request status and command
+    const updatedLeave = await Service.Service_update(user_id, {
+      Leavetype,
+      Command,
+      Status,
+    });
+    console.log("updatedLeave",updatedLeave);
+
+    if (!updatedLeave) {
+      return res.json({
+        status: "Error",
+        message: "Leave request not found",
+      });
+    }
+    
+
+    // If the leave request is approved, deduct the leaves from the user's record
+    if (Status === "accepted") {
+      const username = updatedLeave.username;
+      const existingStaff = await Staffmodel.findOne({ username });
+
+      if (!existingStaff) {
+        return res.json({
+          status: "Error",
+          message: "Staff record not found",
+        });
+      }
+
+      const user = await Staffmodel.findOne({
+        username: updatedLeave.username,
+      });
+
+      if (!user) {
+        return res.json({
+          status: "Error",
+          message: "User record not found",
+        });
+      }
+      console.log("Leavetype:", Leavetype);
+      console.log("Numberofdays:", Numberofdays);
+      console.log(
+        "Staff member's leave balances:",
+        user.Casualleaves,
+        user.Medicalleaves,
+        user.Menstrualleaves,
+        
+      );
+
+      // Save the updated user record in the "users" collection
+      if (Leavetype === "Menstrualleaves" && user.Gender !== "Female") {
+        return res.json({
+          status: "Error",
+          message: "Menstrual leave is only applicable for women",
+        });
+      }
+
+      // Deduct the appropriate leave balance based on the leave type
+      if (Leavetype === "Casualleaves") {
+        user.Casualleaves -= Numberofdays;
+      } else if (Leavetype === "Medicalleaves") {
+        user.Medicalleaves -= Numberofdays;
+      } else if (Leavetype === "Menstrualleaves") {
+        user.Menstrualleaves -= Numberofdays;
+      } else {
+        return res.json({
+          status: "Error",
+          message: "Invalid leave type",
+        });
+      }
+
+      if (
+        user.Casualleaves < 0 ||
+        user.Medicalleaves < 0 ||
+        user.Menstrualleaves < 0
+      ) {
+        return res.json({
+          status: "Error",
+          message: "Insufficient leave balance",
+        });
+      }
+
+      await user.save();
+     
+      // If the leave request is approved, deduct the leaves from the user's record
+      if (Status === "accepted") {
+        
+
+        const userEmail = user.email; // Assuming the staff member has an 'email' field
+        const notificationSubject = "Leave Request Approved";
+        const notificationText = "Your leave request has been approved.";
+
+        // Send email notification for leave approval
+        sendEmail(userEmail, notificationSubject, notificationText);
+      }
+    //   return;
+    }
+    if (Status === "rejected") {
+      // Update the leave request status to "rejected" in the database
+      /*const leaveUpdate = await Leavemodel.findByIdAndUpdate(user_id, {
+        Command,
+        Status: "rejected",
+        
+      });
+       { new: true }).lean();*/
+      const leave = await Leavemodel.findByIdAndUpdate(
+        user_id,
+        { Command, Status: "rejected" },
+        { new: true }
+      ).lean();
+
+      if (!leave) {
+        return res.json({
+          status: "Error",
+          message: "Failed to update leave request status to rejected",
+        });
+      }
+      const user = await Staffmodel.findOne({
+        username: updatedLeave.username,
+      });
+
+      if (!user) {
+        return res.json({
+          status: "Error",
+          message: "User record not found",
+        });
+      }
+
+      // Send email notification for leave rejection
+      const userEmail = user.email;
+      const notificationSubject = "Leave Request Rejected";
+      const notificationText = "Your leave request has been rejected.";
+
+      try {
+        await sendEmail(userEmail, notificationSubject, notificationText);
+
+        // Query the updated leave data for the response
+        const leave = await Leavemodel.findById(user_id);
+        res.json({
+          status: "Success",
+          message: "Leave request rejected successfully",
+          data: leave,
+        });
+      } catch (emailError) {
+        console.error("Error sending rejection email:", emailError);
+         res.json({
+          status: "Error",
+          message: "Error sending email for leave rejection",
+        });
+      }
+    }
+
+    // Update for accepted leaves or any other logic
+
+    const leave = await Leavemodel.findByIdAndUpdate(
+      user_id,
+      {
+        Command,
+        Status,
+      },
+      { new: true }
+    );
+    
+    res.json({
+      status: "Success",
+      message: "Leave request updated successfully",
+      data: leave,
+    });
+  } 
+  catch (error) {
+    return error;
   }
 };
 
