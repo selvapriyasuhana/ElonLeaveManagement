1e375bd0f0b36dfad245459758d0aced3c3b4bbc
selvapriyasuhana <142295945+selvapriyasuhana@users.noreply.github.com>
2023-12-05 14:59:36 +0530

    Add files via upload

---
+++ Models/WorkingHoursModel.js
@@ -0,0 +1,32 @@
+const SECONDS_IN_HOUR = 3600;
+const SECONDS_IN_MINUTE = 60;
+const mongoose = require("mongoose");
+
+const workingHoursSchema = new mongoose.Schema({
+  staff: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Staffdetails",
+  },
+  checkinTime: {
+    type: Date,
+    required: true,
+    
+  },
+  checkoutTime: {
+    type: Date,
+    
+  },
+  workingHours: {
+    type: String,
+    default: 0,
+    
+  },
+  date: {
+    type: Date, // Include the date field
+    default: Date.now,
+  },
+});
+
+const WorkingHours = mongoose.model("WorkingHours", workingHoursSchema);
+
+module.exports = WorkingHours;
