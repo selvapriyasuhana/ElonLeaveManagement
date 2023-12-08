4ea50a542580bfb977217bbe371dbf4ad1eabfb0
selvapriyasuhana <142295945+selvapriyasuhana@users.noreply.github.com>
2023-12-08 17:35:32 +0530

    Update controller.js

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,37 +1,102 @@
-Service = require('../Service/service.js');
+const Service = require('../Service/service');
 
-exports.index = function(err,user)
-{
-    Service.Service_index(err,user);
-    if (err) return console.error(err);
+exports.index = async function (req, res) {
+  try {
+    const user = await Service.Service_index();
+    res.json({
+      status: "Success",
+      message: " All Assets details retrieved Successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
 };
 
-exports.add = function(err,user)
-{
-    Service.Service_add(err,user);
-    if (err) return console.error(err);
-}
+exports.add = async function (req, res) {
+  try {
+    // Assuming you have the data to add in the request body
+    const user = req.body;
+    const data = await Service.Service_add(user);
+    res.status(201).json({
+      status: "Success",
+      message: "User added successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
+};
 
-exports.view = function(err,user)
-{
-    Service.Service_view(err,user);
-    if (err) console.error(err);
-}
+exports.view = async function (req, res) {
+  try {
+    const user_id = req.params.user_id;
+    const user = await Service.Service_view(user_id);
+    res.json({
+      status: "Success",
+      message: "Given _id Assetdetails retrieved successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
+};
 
-exports.see = function(err,user)
-{
-    Service.Service_see(err,user);
-    if (err) console.error(err);
-}
+exports.see = async function (req, res) {
+  try {
+    const users = await Service.Service_see();
+    res.json({
+      status: "Success",
+      message: "Got all user details Successfully",
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
+};
 
-exports.update = function(err,user)
-{
-    Service.Service_update(err,user);
-    if (err) console.error(err);
-}
+exports.update = async function (req, res) {
+  try {
+    const user_id = req.params.user_id;
+    const data = req.body;
+    const user = await Service.Service_update(user_id, data);
+    res.json({
+      status: "Success",
+      message: "Given  _id Asset details updated successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
+};
 
-exports.Delete = function(err,user)
-{
-    Service.Service_Delete(err,user);
-    if (err) console.error(err);
-}
+exports.Delete = async function (req, res) {
+  try {
+    const user_id = req.params.user_id;
+    await Service.Service_Delete(user_id);
+    res.json({
+      status: "Success",
+      message: "User details deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
+};
