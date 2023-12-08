3a4af994fbc89c7e6152cb10af6075a1d6b99e0c
selvapriyasuhana <142295945+selvapriyasuhana@users.noreply.github.com>
2023-12-08 17:36:12 +0530

    Update assigncontroller.js

--- Controller/assigncontroller.js
+++ Controller/assigncontroller.js
@@ -1,37 +1,89 @@
-Service = require('../Service/assignservice.js');
+const Service = require('../Service/assignservice.js');
 
-exports.index = function(err,user)
-{
-    Service.Service_index(err,user);
-    if (err) return console.error(err);
-};
+exports.index = async function (req, res) {
+    try {
+      const user = await Service.Service_index();
+      res.json({
+        status: "Success",
+        message: " All Assignedasset details got by Successfully",
+        data: user,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "Error",
+        message: error.message,
+      });
+    }
+  };
+  
+  exports.add = async function (req, res) {
+    try {
+      // Assuming you have the data to add in the request body
+      const user = req.body;
+      const data = await Service.Service_add(user);
+      res.status(201).json({
+        status: "Success",
+        message: "User added successfully",
+        data: user,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "Error",
+        message: error.message,
+      });
+    }
+  };
+  
+  exports.view = async function (req, res) {
+    try {
+      const user_id = req.params.user_id;
+      const user = await Service.Service_view(user_id);
+      res.json({
+        status: "Success",
+        message: "Assetassign details retrieved by _id successfully",
+        data: user,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "Error",
+        message: error.message,
+      });
+    }
+  };
+  
 
-exports.add = function(err,user)
-{
-    Service.Service_add(err,user);
-    if (err) return console.error(err);
-}
-
-exports.view = function(err,user)
-{
-    Service.Service_view(err,user);
-    if (err) console.error(err);
-}
-
-exports.see = function(err,user)
-{
-    Service.Service_see(err,user);
-    if (err) console.error(err);
-}
-
-exports.update = function(err,user)
-{
-    Service.Service_update(err,user);
-    if (err) console.error(err);
-}
-
-exports.Delete = function(err,user)
-{
-    Service.Service_Delete(err,user);
-    if (err) console.error(err);
-}
+  
+  exports.update = async function (req, res) {
+    try {
+      const user_id = req.params.user_id;
+      const data = req.body;
+      const user = await Service.Service_update(user_id, data);
+      res.json({
+        status: "Success",
+        message: "Given _id Assignedasset details updated successfully",
+        data: user,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "Error",
+        message: error.message,
+      });
+    }
+  };
+  
+  exports.Delete = async function (req, res) {
+    try {
+      const user_id = req.params.user_id;
+      await Service.Service_Delete(user_id);
+      res.json({
+        status: "Success",
+        message: "User details deleted successfully",
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: "Error",
+        message: error.message,
+      });
+    }
+  };
+  
